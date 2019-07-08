f52107f3bc7be7b053568f433ef79cdfcde98b26
fdc-isai <fdc.gesapine@gmail.com>
2019-07-08 09:47:23 +0800

    test 5

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -31,7 +31,7 @@
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 	Router::connect('/posts/', array('controller' => 'posts', 'action' => 'index'));
-	Router::connect('/testtest2', array('controller' => 'posts', 'action' => 'testtest2'));
+	Router::connect('/testtest3', array('controller' => 'posts', 'action' => 'testtest3'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
