5558a1bd147452e9e3531b59a01827c5415cedee
fdc-isai <fdc.gesapine@gmail.com>
2019-07-11 09:21:55 +0800

    to fail

--- app/View/Posts/index.php
+++ app/View/Posts/index.php
@@ -133,7 +133,7 @@ _lt('send', 'pv', ['1e11feb8-0b70-4f0b-b0b2-826a83b5721d']);
 </script>
 <!-- END Rakuten Marketing Tracking -->
 </head>
-<body <?php echo (isset($showLink) && $showLink)? 'class="is-mobile"': ''; ?> ng-app="userApp" ng-cloak>
+<body <?php echo (isset($showLink) && $showLink) 'class="is-mobile"': ''; ?> ng-app="userApp" ng-cloak>
 <?php $ua = $_SERVER['HTTP_USER_AGENT'];?>
 
 <!-- Google Tag Manager -->
@@ -799,7 +799,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <a href="#" class="top_link" id="top_link" data-scroll="pageTop" style="display: block;"><i class="fa fa-arrow-up"></i></a>
 </div>
 <!-- // .page_wrap -->
-<?php if(isset($maintenance)) : ?>
+<?php if(isset($maintenance)): ?>
     <?php echo $this->element('footer_maintenance') ?>
 <?php else: ?>
     <?php if(!isset($lessons)) : ?><?php echo $this->element('footer_2') ?><?php endif ?>
